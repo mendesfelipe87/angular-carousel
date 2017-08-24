3701c6de98a702da557f814b7a5d81c3803c173d
Felipe Mendes <felipe.luis@lexisnexisrisk.com>
2017-08-24 09:56:57 -0300

    prevent swipe first to right and last to left

--- src/directives/rn-carousel.js
+++ src/directives/rn-carousel.js
@@ -359,7 +359,7 @@
                                         return;
                                     }
                                 }
-                                
+
                                 updateContainerWidth();
                                 elX = iElement[0].querySelector('li').getBoundingClientRect().left;
                                 pressed = true;
@@ -373,6 +373,26 @@
                                 bindMouseUpEvent();
                                 if (pressed) {
                                     x = coords.x;
+
+                                    var carouselElement = document.querySelectorAll("[rn-carousel]")[0];
+
+                                    if (carouselElement) {
+                                        var loop = carouselElement.getAttribute("rn-carousel-loop");
+
+                                        if (loop === "false") {
+                                            var swipeRight = startX < x;
+                                            var swipeLeft = startX > x;
+
+                                            if (swipeRight && event.target.closest("li").id === currentSlides[0].id.toString()) {
+                                                return;
+                                            }
+
+                                            if (swipeLeft && event.target.closest("li").id === currentSlides[currentSlides.length - 1].id.toString()) {
+                                                return;
+                                            }
+                                        }
+                                    }
+
                                     delta = startX - x;
                                     if (delta > 2 || delta < -2) {
                                         swipeMoved = true;
